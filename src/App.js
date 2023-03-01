c3b7d760f3d192c159dcf8fcd9a78261ff4f79cc
Duong-Khang <duonghoangkhang28032000@gmail.com>
2023-03-01 18:46:51 +0700

    March_1_2023_95

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Fragment } from 'react';
+import { publicRoutes } from '~/Routes';
+import { DefaultLayout } from '~/components/Layout';
+
 function App() {
     return (
-        <div className="App">
-            <h1>Hello React</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes.map((route, index) => {
+                        const Page = route.component;
+                        let Layout = DefaultLayout;
+
+                        if (route.layout === null) {
+                            Layout = Fragment;
+                        } else if (route.layout) {
+                            Layout = route.layout;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/pages/Search/index.js
@@ -0,0 +1,11 @@
+function Search() {
+    return (
+        <>
+            <div className="content">
+                <h2>Search</h2>
+            </div>
+        </>
+    );
+}
+
+export default Search;
